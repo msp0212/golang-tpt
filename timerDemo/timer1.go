16a29e1f3ca3c53547382742a34c252101ef94d5
Mohit Singh <mohit.singh@quickheal.com>
2025-03-25 17:48:16 +0530

    fixed ticker demo

--- timerDemo/timer1.go
+++ timerDemo/timer1.go
@@ -14,18 +14,24 @@ time.NewTicker creatse a new ticker(recurring).
 Represent a periodic event in future
 */
 
-func startTimer(timeoutSec int)
-{
+func startTimer(timeoutSec int) {
 	fmt.Println("Will wake you up after", timeoutSec, "seconds")
 	timer := time.NewTimer(time.Second * time.Duration(timeoutSec))
 
 	go func() {
 		select {
 		case t := <- timer.C:
+			fmt.Println("Waking you up after", timeoutSec, "seconds at", t)
+			return
+		case <- time.After(time.Second * 3):
+			// all timers > 3 secs will not be run :)
+			stop := timer.Stop()
+			if stop {
+				fmt.Println("Timer stopped")
+			}
 			return
 		}
-	}
-	fmt.Println("Waking you up after", timeoutSec, "seconds at", t)
+	}()
 }
 
 // wake me up after specified seconds
@@ -33,22 +39,29 @@ func wakeMeUp(timeoutSec int) {
 	fmt.Println("===Timer Demo===")
 	startTimer(1)
 	startTimer(5)
+	time.Sleep(time.Second * 5)
 	fmt.Println("===Timer Demo done===")
 }
 
 // ping me periodically
 func pingMePeriodically(timeoutSec int) {
+	fmt.Println("===Ticker Demo===")
 	ticker := time.NewTicker(time.Second * time.Duration(timeoutSec))
-	for {
-		select {
-		case t := <- ticker.C:
-			fmt.Println("Pinging you up after", timeoutSec, "seconds at", t)
-		case t := <- time.After(time.Second * 10):
-			fmt.Println("Stopping the pings now at", t)
-			return
-		default:
+	timer := time.After(time.Second * 3)
+	go func() {
+		for {
+			select {
+			case t1 := <- timer:
+				fmt.Println("Stopping the pings now", t1)
+				ticker.Stop()
+				return
+			case t2 := <- ticker.C:
+				fmt.Println("Ping at", t2)
+			}
 		}
-	}
+	}()
+	time.Sleep(time.Second * 4)
+	fmt.Println("===Ticker Demo Done===")
 }
 
 func main() {
